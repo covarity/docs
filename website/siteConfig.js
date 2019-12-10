16c273458d24e2d5c68d6483564c663b812079aa
Ben Ebsworth <ben.ebsworth@gmail.com>
2019-12-10 12:10:36 +1100

    adding documentation

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'anchor', // Title for your website.
   tagline: 'Integration Testing for Cloud Native tools',
   url: 'https://covarity.github.io', // Your website URL
-  baseUrl: '/docs', // Base URL for your project */
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
